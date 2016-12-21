283704e287d6319269b7ea800b99648a091d3f36
Ran G <greeran@gmail.com>
2016-12-22 00:04:37 +0200

    fixing http inputs

--- WebServer.lua
+++ WebServer.lua
@@ -7,6 +7,12 @@ if srv~=nil then
   srv:close()
 end
 
+tempMin=25;
+tempMax=35;
+humiMin=25;
+humiMax=35;
+sndToMail="enter mail here"
+
 srv=net.createServer(net.TCP)
 srv:listen(80,function(conn)
     conn:on("receive", function(client,request)
@@ -20,7 +26,11 @@ srv:listen(80,function(conn)
             for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
                 _GET[k] = v
                 print("["..k.."]="..v.." ")
-            end     
+            end
+            for k, v, rr,ss in string.gmatch(vars, "(%w+)=(%w+)%%40(%w+).(%w+)&*") do     
+                print("["..k.."]="..v.."@"..rr.."."..ss)
+                _GET[k]=""..v.."@"..rr.."."..ss;
+            end
         end
 
         if(_GET.getTemp == "Temp")then
@@ -29,10 +39,19 @@ srv:listen(80,function(conn)
         ---      print("Got Thing");
         elseif(_GET.action == "Test")then
           print("Testing mail 2")
+          sndToMail=_GET.notification;
+          tempMax=_GET.maxtemp;
+          tempMin=_GET.mintemp;
+          humiMax=_GET.maxhumd;
+          humiMin=_GET.minhumd;
           sendGmailObj.sendGmailFunc(_GET.notification)
           ---dofile("sendGmail.lua")
         elseif(_GET.action == "Start")then
-          print("starting with temp min/max".._GET.mintemp.."/".._GET.maxtemp)
+          tempMax=_GET.maxtemp;
+          tempMin=_GET.mintemp;
+          humiMax=_GET.maxhumd;
+          humiMin=_GET.minhumd;
+          sndToMail=_GET.notification;
         end
         
         mail_to = _GET.notification
@@ -45,11 +64,11 @@ srv:listen(80,function(conn)
         buf= buf.."<h2>Temprature "..tempr.." Humidity "..humi.."</h2><p>";
         buf= buf.."<p>Get Tempruture and Humidity <a href=\"?getTemp=Temp\"><button>Press</button></a>&nbsp;</p>";
         ---buf= buf.."<p>Send To Thingspeak <a href=\"?sendThings=Thing\"><button>Press</button></a>&nbsp;</p>";
-        buf= buf.."<form><p>Min Temprature <input type=\"text\" name=\"mintemp\" value=\"30\">&nbsp;</p>";
-        buf= buf.."<p>Max Temprature <input type=\"text\" name=\"maxtemp\" value=\"40\">&nbsp;</p>";
-        buf= buf.."<p>Min Humidity <input type=\"text\" name=\"minhumd\" value=\"30\">&nbsp;</p>";
-        buf= buf.."<p>Max Humitidy <input type=\"text\" name=\"maxhumd\" value=\"40\">&nbsp;</p>";
-        buf= buf.."<p>notification Mail <input name=\"notification\" type=\"text\" /> <input type=\"submit\" name=\"action\" value=\"Test Mail\""
+        buf= buf.."<form><p>Min Temprature <input type=\"text\" name=\"mintemp\" value="..tempMin..">&nbsp;</p>";
+        buf= buf.."<p>Max Temprature <input type=\"text\" name=\"maxtemp\" value="..tempMax..">&nbsp;</p>";
+        buf= buf.."<p>Min Humidity <input type=\"text\" name=\"minhumd\" value="..humiMin..">&nbsp;</p>";
+        buf= buf.."<p>Max Humitidy <input type=\"text\" name=\"maxhumd\" value="..humiMax..">&nbsp;</p>";
+        buf= buf.."<p>notification Mail <input name=\"notification\" type=\"email\" value="..sndToMail.." pattern=\"[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$\"> <input type=\"submit\" name=\"action\" value=\"Test Mail\""
         if(_GET.action == "Test")then
           if(false == NotificationFail)then
             buf= buf.."<font color=red> Failed Mail Test <\font>"
@@ -57,7 +76,7 @@ srv:listen(80,function(conn)
             buf= buf.."<font color=green> Success Mail Test <\font>"
           end
         end
-        buf= buf.."</p><p><input type=\"submit\" name=\"action\" value=\"Start Working\" </p></a></form>&nbsp;</p>";
+        buf= buf.."</p><p><input type=\"submit\" name=\"action\" value=\"Start Working\" </sp></a></form>&nbsp;</p>";
 
 
         
